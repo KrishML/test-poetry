8e08e978a13fff5d32e43d2ab44af2cd3f8de4f9
Krishnendu Sanyal <krishnendusanyal95@gmail.com>
2025-08-07 02:01:51 +0100

    added median method

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ def average(nums):
         return 0
     return sum(nums) / len(nums)
 
+def median(nums):
+    """Calculate the median of a list of integers."""
+    n = len(nums)
+    if n == 0:
+        return 0
+    nums_sorted = sorted(nums)
+    mid = n // 2
+    if n % 2 == 0:
+        return (nums_sorted[mid - 1] + nums_sorted[mid]) / 2
+    else:
+        return nums_sorted[mid]
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
@@ -13,5 +25,7 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm says hello')
     numbers = [10, 20, 30, 40, 50]
-    avg = average(numbers)
-    print(f'The average of {numbers} is {avg}')
+    # avg = average(numbers)
+    # print(f'The average of {numbers} is {avg}')
+    med = median(numbers)
+    print(f'The median of {numbers} is {med}')
